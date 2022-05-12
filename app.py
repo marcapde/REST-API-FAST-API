6cf2d1e40bc84d7aee9dfc11c2d49c23033d1923
Marc Capde <capasc5@gmail.com>
2022-05-12 23:44:07 +0200

    only missing get/file

--- app.py
+++ app.py
@@ -5,10 +5,9 @@ import string
 from tkinter import image_types
 from typing import List, Optional
 from unicodedata import name
-import uuid
-from fastapi import FastAPI
+from fastapi import FastAPI, Response
 from pydantic import BaseModel
-from PIL import Image
+from fastapi import File, UploadFile
 
 import random
 app=FastAPI(title='REST API')
@@ -31,6 +30,10 @@ class gallery (BaseModel):
 ##########CLASSES###############
 
 galleries = []
+#for more efficiency as probably we will repaet oprations with the same gallery
+#we will save always de last gallery viewed.
+cache = {"id":-1} 
+#For even more efficiency it could also be implemented with an Image cache
 
 #defining main page
 @app.get('/')
@@ -51,7 +54,7 @@ async def get_galleries():
 async def post_gallery(prop:gallery):
     #to make it comfy for testing id will be a random int within this small range
     #for more scalability use uuid instead.
-    prop.id=random.randint(0,9999)
+    prop.id=random.randint(0,99999)
     galleries.append(dict(prop))
     return prop.id
 
@@ -59,12 +62,23 @@ async def post_gallery(prop:gallery):
 
 @app.get('/galleries/{g_id}')
 async def get_gallery(g_id: int):
+    global cache 
+    if (cache !=None and cache.get("id")==g_id):
+        return dict(name=g.get("name"),desc=g.get("desc"))
+    #else
     for g in galleries:
-        if(g.id==g_id):
+        if(g.get("id")==g_id):
+            cache = g
             return dict(name=g.get("name"),desc=g.get("desc"))
 
 @app.delete('/galleries/{g_id}')
 async def del_gallery(g_id: int):
+    global cache 
+
+    if (cache != None and cache.get("id")==g_id):
+        galleries.remove(cache)
+        return g_id
+    #else
     for g in galleries:
         if(g.get("id")==g_id):
             galleries.remove(g)
@@ -74,6 +88,8 @@ async def del_gallery(g_id: int):
 
 @app.get('/galleries/{g_id}/images')
 async def get_images(g_id: int):
+    global cache 
+
     result =[]
     for g in galleries:
         if (g.get("id")==g_id):
@@ -90,34 +106,98 @@ async def get_images(g_id: int):
 
 @app.post('/galleries/{g_id}/images')
 async def post_images(g_id:int,image:image):
+    global cache 
+
     #to make it comfy for testing id will be a random int within this small range
     #for more scalability use uuid instead.
-    image.id=random.randint(0,9999)
+    image.id=random.randint(0,99999)
+    if (cache != None and cache.get("id")==g_id):
+        g = cache
+    else: 
+        for g in galleries:
+            if (g.get("id")==g_id):
+                cache = g
+                break
+    if (g["images"]!= None):
+        g["images"].append(image)               
+    else:                
+        g["images"]=[image]
+    
+    return image.id
+
+############/galleries/{g_id}/images/{i_id}###################
+
+@app.get('/galleries/{g_id}/images/{i_id}')
+def get_image_byId(g_id:int,i_id:int):
+    global cache 
+    if (cache != None and cache.get("id")==g_id):
+        g = cache
+    else: 
+        for g in galleries:
+            if (g.get("id")==g_id):
+                cache = g
+                break
+    for i in g["images"]:
+        if(i.id == i_id):
+            aux = dict(i)               
+            return (dict(name=aux["name"],
+                desc=aux["desc"],
+                location=aux["location"],
+                width=aux["width"],
+                height=aux["height"]))
+    #if not found
+    return {"405":"image not found"}
+
+
+
+@app.delete('/galleries/{g_id}/images/{i_id}')
+def del_image_byId(g_id:int,i_id:int):   
+    index = 0
     for g in galleries:
         if (g.get("id")==g_id):
-            print(g["images"])
-            if (g["images"]!= None):
-                print(image)
-                g["images"].append(image)
-                print(g["images"]) 
-                #lista = g["images"]
-    	        #print(g["images"])               
-                #g["images"]=lista                
-            else:                
-                g["images"]=[image]
-            print(g["images"])
+            break
+        index+=1
+    for i in g["images"]:
+        if(i.id == i_id):
+            galleries[index]["images"].remove(i)
+            return i_id
+    #if not found
+    return {"405":"image not found"}
+
+
+###########/galleries/{g_id}/images/{i_id}/file################
+#defining galleries page
+
+
+@app.post('/galleries/{g_id}/images/{i_id}/file')
+async def post_file(g_id:int, i_id:int, file: bytes = File(...)):
+    #image(name="A",desc="B",location="C",width=3, height=3, img= str(file))
+    index = 0
+    for g in galleries:
+        if (g.get("id")==g_id):
+            cache = g
+            break
+        index+=1
+    ii = 0
+    for i in g["images"]:
+        if(i.id == i_id):
+            i.img = file
+            galleries[index]["images"][ii] = i
+            return Response(i.img)
+        ii+=1
+    #if not found
+    return {"405":"image not found"}
+
+@app.get('/galleries/{g_id}/images/{i_id}/file')
+async def get_file(g_id:int, i_id:int):
+    index = 0
+    for g in galleries:
+        if (g.get("id")==g_id):
+            cache = g
+            break
+        index+=1
+    for i in g["images"]:
+        if(i.id == i_id):
+            return Response(galleries[index]["images"]["img"])
             
-            return image.id
-
-###############################
-
-#defining galleries page
-@app.get('/galleries/{g_id}/images/{i_id}')
-def get_image():
-    return {"WELCOME":"Welcome to my API"}
-
-
-#defining galleries page
-@app.get('/galleries/{g_id}/images/{i_id}/fle')
-def root():
-    return {"WELCOME":"Welcome to my API"}
+    return {"405":"image not found"}
